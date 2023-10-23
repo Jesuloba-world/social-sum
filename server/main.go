6828c60e28897d60bace0e9b34df0fb649f5f705
Jesuloba <jesulobajohn@gmail.com>
2023-10-23 15:19:10 +0100

    server cors setup

--- server/main.go
+++ server/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
 func main() {
-	println("hello world")
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders: "Content-Type, Authorization",
+	}))
+
+	app.Listen(":8000")
 }
